3b53fd5436328bc89ac9609529497f973e3c575e
Prashant <pryadav84@gmail.com>
2013-05-12 11:45:23 -0400

    05/12

--- app/controllers/Timesheet.java
+++ app/controllers/Timesheet.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import play.*;
@@ -10,10 +12,35 @@ import static play.data.Form.*;
 import views.html.*;
 import models.*;
 
+@Security.Authenticated(Secured.class)
 public class Timesheet extends Controller{
-
-	@Security.Authenticated(Secured.class)
-    public static Result time() {
-    	return ok(time.render(User.findByEmail(request().username())));
+	
+	static Calendar startCal1 = Calendar.getInstance();
+	
+	public static Result time() {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+		Calendar startCal = Calendar.getInstance();
+		Calendar weekFirstDayCalObj = Calendar.getInstance();
+		weekFirstDayCalObj.set(Calendar.DAY_OF_WEEK, weekFirstDayCalObj.getFirstDayOfWeek());	
+		startCal.setTime(weekFirstDayCalObj.getTime());
+    	return ok(time.render(User.findByEmail(request().username()), startCal, "Enter Time"));
     }
+	
+    public static Result approvedTime() {
+    	return ok(time.render(User.findByEmail(request().username()), startCal1, "Approved Time"));
+    }
+    
+    public static Result pendingTime() {
+    	return ok(time.render(User.findByEmail(request().username()), startCal1, "Pending Time"));
+    }
+    
+    public static Result rejectedTime() {
+    	return ok(time.render(User.findByEmail(request().username()), startCal1, "Rejected Time"));
+    }
+    
+    public static Result approveTime() {
+    	return ok(time.render(User.findByEmail(request().username()), startCal1, "Approve Time"));
+    }
+    
 }
--- public/javascripts/custom.js
+++
@@ -1,6 +0,0 @@
-$('#registerHere input').on('mouseenter',function() {
-	$(this).popover('show')
-});
-$('#registerHere input').on('mouseleave',function() {
-	$(this).popover('hide')
-});
